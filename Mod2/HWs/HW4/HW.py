9f63efcbaf435df24a2554646e4d28087df2fe4e
MisterReds <mister.reds666@gmail.com>
2024-01-07 12:27:23 +1100

    HW4

---
+++ Mod2/HWs/HW4/HW.py
@@ -0,0 +1,21 @@
+# импортируем библиотеку pandas
+import pandas as pd
+
+# указываем ссылку на датасет
+
+
+
+# читаем датасет в датафрейм
+df = pd.read_csv('Customers.csv', sep=';')
+print(df.columns.tolist())
+# фильтруем датафрейм по первому условию
+df_filtered_1 = df.query("Age > 30 and Income < 30000")
+
+# фильтруем датафрейм по второму условию
+df_filtered_2 = df.query("Profession == 'Lawyer' and Work_Experience > 5") # удаляем лишнюю букву l
+
+# выводим результаты
+print("Люди, у которых возраст больше 30 и доход меньше 30000:")
+print(df_filtered_1)
+print("Люди, которые по профессии юристы и с опытом работы больше 5 лет:")
+print(df_filtered_2)
